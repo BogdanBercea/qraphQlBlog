bbbf1eb2dcaf511925218866f1792fb130052088
Bercea <bogdanbercea6@gmail.com>
2021-12-15 10:50:32 +0200

    Add vue and tailwind UI dependecies

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,12 +6,14 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
+mix.postCss("resources/css/app.css", "public/css", [
+    require("tailwindcss"),
+]);
+
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .vue()
